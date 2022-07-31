b19150408b36635af146679de474dca11b58e649
jerrylaikr <kerui.lai@mail.mcgill.ca>
2022-07-31 02:57:10 +0000

    move ideas to README

--- wbac/__main__.py
+++ wbac/__main__.py
@@ -4,50 +4,6 @@ import time
 
 SLEEP_TIME = 1200
 
-"""
-每20分钟抓取新微博
-每12小时进行一次回溯检测，检查任何内容存在变动的微博（转发的原博被夹）
-暂时不确定怎么检测被夹的转发和原创
-考虑放在一个新collection里面
-Modified case:
-    weibo
-    {
-        id:"a",
-        content:"AA",
-    }
-
-    weibo_revisited
-    {
-        id:"a",
-        content:"BB",
-    }
-
-Deleted case:
-    weibo
-    {
-        id:"a",
-        content:"BB",
-    }
-
-    weibo_revisited
-    {}
-
-如果距离发布日期已过去24小时，则从数据库中移除:
-OK case (to be removed from weibo coll) (at 2022-07-23 13:01):
-    weibo
-    {
-        id:"a",
-        content:"AA",
-        publish_time:"2022-07-22 13:00",
-    }
-
-    weibo_revisited
-    {
-        id:"a",
-        content:"AA",
-        publish_time:"2022-07-22 13:00",
-    }
-"""
 
 if __name__ == "__main__":
     while True:
