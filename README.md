b32404e62005667a10174b078224850e9e484499
jerrylaikr <kerui.lai@mail.mcgill.ca>
2022-08-01 05:12:20 +0000

    prototype with DB upload part done

--- README.md
+++ README.md
@@ -67,6 +67,12 @@ coll = weibo_rev
 }
 ```
 
+现在需要实现的功能有：
+- 允许将 config_file 作为 weibo_spider 的 argument
+- 指定 collection in MongoDB
+- 删除两个 collections 的交集
+
+
 ## Example
 
 2022-07-20 13:00 开始运行
@@ -79,7 +85,7 @@ coll = weibo_rev
 
 2022-07-22 13:00 回溯检测，爬取48小时前至24小时前（2022-07-20 13:00 - 2022-07-21 13:00）的关注列表微博,存入`weibo_rev` collection。
 
-2022-07-22 13:00 删除`weibo`和`weibo_rev`两个collections的并集。
+2022-07-22 13:00 删除`weibo`和`weibo_rev`两个collections的交集。
 
 此时`weibo_rev` collection中没有doc。
 
@@ -98,6 +104,9 @@ coll = weibo_rev
 即是每次回溯检测只需要爬取20分钟区间的内容。
 
 根据`publish_time`顺序排序，逐个检查，如果内容没有发生变化则删除，如果`publish_time`时间差小于24小时则停止检测。
+
+又或者：将主程序分为两个并行的脚本，脚本0使用`user_id_list.txt`设置文件（可以自动更新since date），脚本1使用整数范围日期（但是不知道怎么设置end date）。
+
 
 ## Run script
 
--- wbac/__main__.py
+++ wbac/__main__.py
@@ -1,20 +1,80 @@
-from weibo_spider.spider import main
-from absl import app
+from datetime import date, datetime, timedelta
+from weibo_spider.spider import Spider, _get_config
+from weibo_spider import config_util, datetime_util
+import sys
+import os
+import json
+from absl.app import parse_flags_with_usage, _run_init, _init_callbacks
 import time
+import logging
+from wbac.spider_rev import main_rev
+import pytz  # TZ CHANGE
 
-SLEEP_TIME = 1200
+
+REV_INTERVAL = timedelta(minutes=30)
+SLEEP_TIME = 600
+logger = logging.getLogger("spider")
+
+
+def remove_unchanged():
+    print("removing unchanged weibo...")
+    pass
+
+
+def main(_):
+    try:
+        config = _get_config()
+        config_util.validate_config(config)
+        wb = Spider(config)
+        wb.start()  # 爬取微博信息
+    except Exception as e:
+        raise
+        sys.exit()
+
+
+def run(
+    main,
+    argv=None,
+    flags_parser=parse_flags_with_usage,
+):
+    args = _run_init(
+        sys.argv if argv is None else argv,
+        flags_parser,
+    )
+    while _init_callbacks:
+        callback = _init_callbacks.popleft()
+        callback()
+    main(args)
 
 
 if __name__ == "__main__":
+    rev_range_since = datetime.now().astimezone(pytz.timezone("PRC"))  # TZ CHANGE
+
+    # update since_date to time now
+    # first fetch will happen after SLEEP_TIME
+    config_path = os.getcwd() + os.sep + "config.json"
+    with open(config_path) as f:
+        config = json.loads(f.read())
+    config["since_date"] = rev_range_since.strftime("%Y-%m-%d %H:%M")
+    config["end_date"] = "now"
+    with open(config_path, "w") as f:
+        json.dump(config, f, indent=4)
+
+    # start running
     while True:
-        # reset since_date
-
-        # Run weibo_spider
-        try:
-            print("Running weibo_spider...")
-            app.run(main)
-        except Exception as e:
-            print("ERROR")
-
         print(f"Sleeping for {SLEEP_TIME}s...")
         time.sleep(SLEEP_TIME)
+
+        # Run weibo_spider
+        run(main)
+
+        # If REV_INTERVAL passed since last rev, run rev
+        now = datetime.now().astimezone(pytz.timezone("PRC"))  # TZ CHANGE
+        rev_range_end = rev_range_since + REV_INTERVAL
+        if now > rev_range_end + REV_INTERVAL:
+            main_rev(
+                rev_range_since.strftime("%Y-%m-%d %H:%M"),
+                rev_range_end.strftime("%Y-%m-%d %H:%M"),
+            )
+            remove_unchanged()
+            rev_range_since += REV_INTERVAL
